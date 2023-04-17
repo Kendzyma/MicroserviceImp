5165cb36e4b2da38cc0c6773429d661593032981
kehinde tiamiyu <kehinde.tiamiyu@unionsystems.com>
2023-04-17 05:49:02 +0100

    micro

---
+++ inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,33 @@
+package com.example.inventoryservice;
+
+import com.example.inventoryservice.model.Inventory;
+import com.example.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+@Bean
+public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13");
+			inventory.setQuantity(100);
+
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("iphone_13_red");
+			inventory2.setQuantity(1);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory2);
+		};
+}
+}
